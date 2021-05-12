ac7407d3eb135978f09ac8613f08edefd5767b75
EroAuditore <alfa.fenrir@gmail.com>
2021-05-12 14:41:55 -0500

    Pending friends array to query model change

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,13 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+   # Users who have yet to confirme friend requests
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
   has_many :friends, through: :confirmed_friendships
+
+  # Users who need to confirm friendship
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
@@ -21,10 +26,7 @@ class User < ApplicationRecord
     friends_array.compact
   end
 
-  # Users who have yet to confirme friend requests
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
+  
 
   # Users who have requested to be friends
   def friend_requests
@@ -33,10 +35,10 @@ class User < ApplicationRecord
 
   # To acept invitation to friendship
   def confirm_friend(user)
-    self.update_attributes(confirmed: true)
-    #friendship = inverse_friendships.find { |friendship| friendship.user == user }
-    #friendship.confirmed = true
-    #friendship.save
+    
+    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship.confirmed = true
+    friendship.save
   end
 
   # To reject invitation to friendship
